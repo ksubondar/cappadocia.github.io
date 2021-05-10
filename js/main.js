fbd9307eddd64197493ab53bbd4d9e62c9609ba7
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-05-10 14:29:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,5 +38,13 @@ $( document ).ready(function() {
 $("[data-fancybox]").fancybox({
     loop: true
      });
-
+$('.button-booking, .nav-link').click(function() {
+    $('html, body').animate({
+scrollTop: $($(this).attr('href')).offset().top + 'px'
+    }, {
+        duration: 1000,
+        easing: 'swing'
+    });
+return false;
+});
 });
